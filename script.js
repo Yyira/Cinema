2fa51e6b2ee434180e76c6f23c9b08e82dc6176a
icaro <irenanlima16@gmail.com>
2024-09-10 15:32:27 -0300

    dt

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-let filmCard = [
-    {
+
+movies = [
+    {   
         title: 'Homem Aranha no Aranhaverso',
         imageIndex: 'img/card-placeholder.jpg'
     },
@@ -18,9 +19,9 @@ function lod() {
     header = document.getElementById('header')
     main.style.paddingTop = `${Number(header.clientHeight)}px`
     cards = document.getElementById('cards')
-    for (c = 0; c < filmCard.length; c++) {
+    for (c = 0; c < movies.length; c++) {
 
-        cards.innerHTML += `<section><div class="leg"><h2>${filmCard[c].title}</h2></div><img src="${filmCard[c].imageIndex}" alt=""></section>`
+        cards.innerHTML += `<section><div class="leg"><h2>${movies[c].title}</h2></div><img src="${movies[c].imageIndex}" alt=""></section>`
     }
 }
 function extend(){
@@ -48,9 +49,9 @@ function unExtend(){
 function search() {
     input = document.getElementById('input')
     cards.innerHTML = ''
-    for (c = 0; c < filmCard.length; c++) {
-        if (filmCard[c].title.toLowerCase().includes(input.value.toLowerCase())) {
-            cards.innerHTML += `<section><div class="leg"><h2>${filmCard[c].title}</h2></div><img src="${filmCard[c].imageIndex}" alt=""></section>`
+    for (c = 0; c < movies.length; c++) {
+        if (movies[c].title.toLowerCase().includes(input.value.toLowerCase())) {
+            cards.innerHTML += `<section><div class="leg"><h2>${movies[c].title}</h2></div><img src="${movies[c].imageIndex}" alt=""></section>`
         }
     }
 
