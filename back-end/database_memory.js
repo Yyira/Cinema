a361284911455b5f358e80e6a166c94e0f620f7f
icaro <irenanlima16@gmail.com>
2024-09-12 14:48:17 -0300

    MOVIES System

--- back-end/database_memory.js
+++ back-end/database_memory.js
@@ -3,9 +3,30 @@ export class DatabaseMemory {
     #movies = new Map
 
     list(){
-        this.#movies.set(1,{name:'miranha1', imageIndex: 'img/card-placeholder.jpg',})
-        this.#movies.set(2,{name:'miranha2',imageIndex: 'img/card-placeholder.jpg',})
-        this.#movies.set(3,{name:'miranha3',imageIndex: 'img/card-placeholder.jpg',})
+        this.#movies.set(1,{
+            name:'miranha1',
+            imageIndex: 'img/card-placeholder.jpg',
+            synopsis: 'Uma aranha muito da doida 1',
+            assessment: 10,
+            director: 'Yira',
+            link: "https://youtu.be/dQw4w9WgXcQ?si=ce1ySmcI3mRMwz3-"
+        })
+        this.#movies.set(2,{
+            name:'miranha2',
+            imageIndex: 'img/card-placeholder.jpg',
+            synopsis: 'Uma aranha muito da doida 2',
+            assessment: 10,
+            director: 'Marcílio',
+            link: "https://youtu.be/dQw4w9WgXcQ?si=ce1ySmcI3mRMwz3-"
+        })
+        this.#movies.set(3,{
+            name:'miranha3',
+            imageIndex: 'img/card-placeholder.jpg',
+            synopsis: 'Uma aranha muito da doida 3',
+            assessment: 10,
+            director: 'Kevyn',
+            link: "https://youtu.be/dQw4w9WgXcQ?si=ce1ySmcI3mRMwz3-"
+        })
         return(Array.from(this.#movies))
     }
     createMovie(movie){
--- front-end/script.js
+++ front-end/script.js
@@ -11,9 +11,9 @@ function loadMainPage() {
 
     fetch(SERVER_URL, { method: "GET" }).then((res) => res.json()).then(movie => {
 
-        for (index = 0; index < movie.length; index++) {
+        for (index = 0; index <= movie.length; index++) {
 
-            cards.innerHTML += `<section><a href="movie.html">
+            cards.innerHTML += `<section><a href="movie.html" onclick="setMovie(${movie[index][0]})">
                 <div class="leg"><h2>${movie[index][1].name}</h2></div><img src="${movie[index][1].imageIndex}" alt="">
             </a></section>`
         }
@@ -64,10 +64,41 @@ function search() {
 //#endregion
 
 //#region funções da pagina dos filmes
-function loadMoviePage(){
+function loadMoviePage() {
     main = document.getElementById('main')
     header = document.getElementById('header')
     main.style.paddingTop = `${Number(header.clientHeight)}px`
-}
+    movieContent = document.getElementById('movieContent')
+    footerLink = document.getElementById('footerLink')
+    id = localStorage.getItem(1)
+    fetch(SERVER_URL, { method: "GET" }).then((res) => res.json()).then(movie => {
 
+
+        for (index = 0; index < movie.length; index++) {
+
+            if (movie[index][0] == id) {
+                movieContent.innerHTML = (`
+                    <h1>${movie[index][1].name}</h1>
+                    <h2>Sinopse:</h2>
+                    <p>${movie[index][1].synopsis}</p>
+                    <h2>Avaliação:${movie[index][1].assessment}/10</h2>
+                    <h2>Diretor:${movie[index][1].director}</h2>
+                    `)
+                footerLink.innerHTML = (
+                    `
+                    <a href="index.html"><button class="back" id="back"><h2>Voltar</h2></button></a>
+        <a href="${movie[index][1].link}" id="youtubeLink"><button class="youtube" id="youtube"><img src="img/youtube icon.png" alt="Youtube"><h2>Trailer no Youtube</h2></button></a>
+                    `
+                )
+            
+            }
+        }
+
+    })
+
+}
+function setMovie(id) {
+    localStorage.setItem(1, id)
+
+}
 //#endregion
